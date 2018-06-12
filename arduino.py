30f9142bc5cf251d36e43f3e623d96f3dae4f143
puto el que lo lea <saramirez@unicauca.edu.co>
2018-06-12 13:00:43 -0500

    Se implemento la clase arduino.py y se asignaron los pines de los leds y el display en main3.py lineas 10 y 12

--- arduino.py
+++ arduino.py
@@ -1,8 +1,23 @@
-#import mraa
+import mraa
+
 def init_leds(*pines):
-	return pines # copia la funcion pines
+	LEDS = []
+	for pin in pines:
+		LED = mraa.Gpio(pin)
+		LED.dir(mraa.DIR_OUT)
+		LEDS.append(LED)
+	return LEDS
+def init_puls(*pines):
+	PULS = []
+	for pin in pines:
+		PUL = mraa.Gpio(pin)
+		PUL.dir(mraa.DIR_IN)
+		PULS.append(PUL)
+	return PULS
 def off_pines(*pines):
-	pass
+	for pin in display:
+		pin.enable(False)
+		pin.write(0)
 class Display():
 	def __init__(self,A,B,C,D,E,F,G):
 		[self.A,self.B,self.C,self.D,self.E,self.F,self.G]= init_leds(A,B,C,D,E,F,G)
@@ -24,24 +39,101 @@ class Display():
 		self.numero[numero]()
 	def cero(self):
 		print "cero"
+		self.A.write(0)
+		self.B.write(0)
+		self.C.write(0)
+		self.D.write(0)
+		self.E.write(0)
+		self.F.write(0)
+		self.G.write(0)
 	def uno(self):
 		print "uno"
+		self.A.write(0)
+		self.B.write(1)
+		self.C.write(1)
+		self.D.write(0)
+		self.E.write(0)
+		self.F.write(0)
+		self.G.write(0)
 		#aqui tenes que hacer el numero uno en el display, solo que ahora no es A sino que self.A
 	def dos(self):
 		print "dos"
+		self.A.write(1)
+		self.B.write(1)
+		self.C.write(0)
+		self.D.write(1)
+		self.E.write(1)
+		self.F.write(0)
+		self.G.write(1)
 	def tres(self):
 		print "tres"
+		self.A.write(1)
+		self.B.write(1)
+		self.C.write(1)
+		self.D.write(1)
+		self.E.write(0)
+		self.F.write(0)
+		self.G.write(1)
 	def cuatro(self):
 		print "cuatro"
+		self.A.write(0)
+		self.B.write(1)
+		self.C.write(1)
+		self.D.write(0)
+		self.E.write(0)
+		self.F.write(1)
+		self.G.write(1)
 	def cinco(self):
 		print "cinco"
+		self.A.write(1)
+		self.B.write(0)
+		self.C.write(1)
+		self.D.write(1)
+		self.E.write(0)
+		self.F.write(1)
+		self.G.write(1)
 	def seis(self):
 		print "seis"
+		self.A.write(1)
+		self.B.write(0)
+		self.C.write(1)
+		self.D.write(1)
+		self.E.write(1)
+		self.F.write(1)
+		self.G.write(1)
 	def siete(self):
 		print "siete"
+		self.A.write(1)
+		self.B.write(1)
+		self.C.write(1)
+		self.D.write(0)
+		self.E.write(0)
+		self.F.write(0)
+		self.G.write(0)
 	def ocho(self):
 		print "ocho"
+		self.A.write(1)
+		self.B.write(1)
+		self.C.write(1)
+		self.D.write(1)
+		self.E.write(1)
+		self.F.write(1)
+		self.G.write(1)
 	def nueve(self):
 		print	 "nueve"
+		self.A.write(1)
+		self.B.write(1)
+		self.C.write(1)
+		self.D.write(0)
+		self.E.write(0)
+		self.F.write(1)
+		self.G.write(1)
 	def diez(self):
 		print "diez"
+		self.A.write(1)
+		self.B.write(1)
+		self.C.write(1)
+		self.D.write(1)
+		self.E.write(1)
+		self.F.write(1)
+		self.G.write(0)
--- main3.py
+++ main3.py
@@ -7,9 +7,9 @@ import arduino
 PORT = 8002
 personas = {"W","X","Y","Z"}
 votos_persona ={"W":1,"X":2,"Y":3,"Z":4}
-LEDS = arduino.init_leds(13,14,15,16)#13,14,15,16 pines de los leds
+LEDS = arduino.init_leds(6,5,4,3)#13,14,15,16 pines de los leds
 LEDS =  dict(zip(personas,LEDS))
-DISPLAY = arduino.Display(0,1,2,3,4,5,6)#pines del display
+DISPLAY = arduino.Display(13,12,11,10,9,7,8)#pines del display
 class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	def do_GET(self):
 		if self.path == '/':
@@ -25,30 +25,26 @@ class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
                      'CONTENT_TYPE':self.headers['Content-Type'],
                      })
 		if "cbxW" in form:
-			#LED["W"].write(True)
+			LED["W"].write(True)
 			total_votos += votos_persona["W"]
 		else:
-			pass ##bora el pass y descomenta la sieguiente linea
-			#LED["W"].write(False)
+			LED["W"].write(False)
 		if "cbxX" in form:
-			#LED["X"].write(True)
+			LED["X"].write(True)
 			total_votos += votos_persona["X"]
 		else:
-			pass ##bora el pass y descomenta la sieguiente linea
-			#LED["X"].write(False)
+			LED["X"].write(False)
 
 		if "cbxY" in form:
-			#LED["Y"].write(True)
+			LED["Y"].write(True)
 			total_votos += votos_persona["Y"]
 		else:
-			pass ##bora el pass y descomenta la sieguiente linea
-			#LED["Y"].write(False)
+			LED["Y"].write(False)
 		if "cbxZ" in form:
-			#LED["Z"].write(True)
+			LED["Z"].write(True)
 			total_votos += votos_persona["Z"]
 		else:
-			pass ##bora el pass y descomenta la sieguiente linea
-			#LED["Z"].write(False)
+			LED["Z"].write(False)
 		DISPLAY.write(total_votos)
 		return self.do_GET()
 
